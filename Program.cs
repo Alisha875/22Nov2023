2fb3debf93afc3f8ac0e84c12ec715afa3f6744d
Alisha Nayak <69793073+Alisha875@users.noreply.github.com>
2023-11-22 15:53:17 +0530

    Programs.cs

--- Program.cs
+++ Program.cs
@@ -10,23 +10,22 @@ namespace Assignment19
     {
         static void Main(string[] args)
         {
-            // Create instances of Smartphone and Laptop classes
+           
             SmartPhone smartphone = new SmartPhone("Apple", "iPhone 12");
             Laptop laptop = new Laptop("Dell", "XPS 13");
 
-            // Set their properties
+           
             smartphone.Brand = "Apple";
             smartphone.Model = "iPhone 12";
             laptop.Brand = "Dell";
             laptop.Model = "XPS 13";
 
-            // Call the methods from each interface
+           
             smartphone.Connect();
             smartphone.Recharge(60);
             laptop.Connect();
             laptop.Recharge(120);
 
-            // Print the information for each device
             Console.WriteLine(smartphone.Display());
             Console.WriteLine(laptop.Display());
             Console.ReadKey();
